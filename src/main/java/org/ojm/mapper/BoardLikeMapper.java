2dd1363cc66a4f3f8acd78badb3fcbe3fd3a3991
hyunwww <114130602+hyunwww@users.noreply.github.com>
2023-08-29 16:24:25 +0900

    Delete src directory

--- src/main/java/org/ojm/mapper/BoardLikeMapper.java
+++
@@ -1,9 +0,0 @@
-package org.ojm.mapper;
-
-import org.ojm.domain.BoardLikeVO;
-
-public interface BoardLikeMapper {
-	public int bCountLike(int bno, int uno);
-	public int bLikeUp(int bno, int uno);
-	public int bLikeDown(int bno, int uno);
-}
